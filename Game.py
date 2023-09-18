c034a342006761aae6df89a64baf7dbd12cffeaa
Stev <stevenvallejo780@gmail.com>
2023-09-17 23:41:13 -0500

    casi completo

--- Game.py
+++ Game.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Constants
 iterations = 1000
 resources_max = 10000
 num_countries = 5
 num_strategies = 10
+num_resources = 4
 event_chance_value = 0.6
 exploration_rate = 0.2
 crisis_chance_value = 0.1
 alliance_chance_value = 0.4
+political_systems = ['dictatorship', 'democracy', 'democracy', 'dictatorship', 'democracy']
 
+# Initialize Q-values
+q_values = np.random.rand(num_countries, num_strategies)
 
 class PoliticalGameSimulation:
     def __init__(self, iterations, num_countries, num_strategies, num_resources, political_systems):
@@ -18,14 +23,12 @@ class PoliticalGameSimulation:
         self.num_strategies = num_strategies
         self.num_resources = num_resources
         self.political_systems = political_systems
-        self.resources = np.random.randint(
-            10, resources_max, (num_countries, num_resources)).astype(float)
+        self.resources = np.random.randint(10, resources_max, (num_countries, num_resources)).astype(float)
         self.strategy_history = np.zeros((iterations, num_countries))
         self.payoff_history = np.zeros((iterations, num_countries))
         self.stability_history = np.zeros((iterations, num_countries))
         self.total_resources_history = np.zeros((iterations, num_countries))
-        self.diplomatic_relations = np.random.randint(
-            -1, 2, (num_countries, num_countries))
+        self.diplomatic_relations = np.random.randint(-1, 2, (num_countries, num_countries))
         self.inflation_rates = np.random.uniform(1, 3, num_countries)
         self.population = np.random.randint(10, 50, num_countries)
 
@@ -65,37 +68,30 @@ class PoliticalGameSimulation:
             event_chance = np.random.rand()
             if event_chance > event_chance_value:
                 event_country = np.random.randint(0, self.num_countries)
-                self.resources[event_country] += np.random.randint(
-                    5, 15, self.num_resources)
+                self.resources[event_country] += np.random.randint(5, 15, self.num_resources)
             elif event_chance < 0.3:
                 event_country = np.random.randint(0, self.num_countries)
-                self.resources[event_country] -= np.random.randint(
-                    5, 15, self.num_resources)
+                self.resources[event_country] -= np.random.randint(5, 15, self.num_resources)
 
             crisis_chance = np.random.rand()
             if crisis_chance > crisis_chance_value:
                 crisis_country = np.random.randint(0, self.num_countries)
-                self.resources[crisis_country] -= np.random.randint(
-                    5, 15, self.num_resources)
+                self.resources[crisis_country] -= np.random.randint(5, 15, self.num_resources)
 
             for j in range(self.num_countries):
                 if self.diplomatic_relations[i, j] > 0:
-                    self.resources[i] += np.random.randint(
-                        0, 3, self.num_resources)
+                    self.resources[i] += np.random.randint(0, 3, self.num_resources)
                 elif self.diplomatic_relations[i, j] < 0:
-                    self.resources[i] -= np.random.randint(
-                        0, 3, self.num_resources)
+                    self.resources[i] -= np.random.randint(0, 3, self.num_resources)
 
             for j in range(i+1, self.num_countries):
                 alliance_chance = np.random.rand()
                 if alliance_chance > alliance_chance_value:
-                    self.resources[i] += np.random.randint(
-                        1, 4, self.num_resources)
-                    self.resources[j] += np.random.randint(
-                        1, 4, self.num_resources)
+                    self.resources[i] += np.random.randint(1, 4, self.num_resources)
+                    self.resources[j] += np.random.randint(1, 4, self.num_resources)
 
-    def run_simulation(self, iterations, q_values):
-        for t in range(iterations):
+    def run_simulation(self):
+        for t in range(self.iterations):
             payoffs = self.calculate_payoffs(q_values)
             self.update_resources()
             self.update_strategies(q_values)
@@ -104,30 +100,20 @@ class PoliticalGameSimulation:
             self.stability_history[t] = np.std(payoffs)
             self.total_resources_history[t] = np.sum(self.resources, axis=1)
 
-
-# Re-running the simulation with the updated class
-political_systems = ['dictatorship', 'democracy',
-                     'democracy', 'dictatorship', 'democracy']
-q_values = np.random.rand(num_countries, num_strategies)
-game = PoliticalGameSimulation(
-    iterations, num_countries, num_strategies, 4, political_systems)
-game.run_simulation(iterations, q_values)
+# Run simulation
+game = PoliticalGameSimulation(iterations, num_countries, num_strategies, num_resources, political_systems)
+game.run_simulation()
 
 # Plotting
-fig, axs = plt.subplots(5, 1, figsize=(12, 30))
-titles = ['Strategies Over Time', 'Payoffs Over Time', 'Stability Over Time',
-          'Total Strategy Changes Over Time', 'Total Resources Over Time']
-labels = ['Strategy', 'Payoff', 'Stability',
-          'Total Changes', 'Total Resources']
+fig, axs = plt.subplots(4, 1, figsize=(12, 24))
+titles = ['Strategies Over Time', 'Payoffs Over Time', 'Stability Over Time', 'Total Resources Over Time']
+labels = ['Strategy', 'Payoff', 'Stability', 'Total Resources']
 
-for i, (history, title, label) in enumerate(zip([game.strategy_history, game.payoff_history, game.stability_history, np.sum(np.abs(np.diff(game.strategy_history, axis=0)), axis=1), np.sum(game.total_resources_history, axis=1)], titles, labels)):
-    axs[i].plot(range(iterations) if i != 3 else range(1, iterations), history if len(
-        history.shape) > 1 else history, label=label if len(history.shape) > 1 else label)
+for i, (history, title, label) in enumerate(zip([game.strategy_history, game.payoff_history, game.stability_history, game.total_resources_history], titles, labels)):
+    axs[i].plot(history if len(history.shape) > 1 else history)
     axs[i].set_title(title)
     axs[i].set_xlabel('Iteration')
     axs[i].set_ylabel(label)
-    if len(history.shape) > 1:
-        axs[i].legend()
 
 plt.tight_layout()
 plt.show()
